6fdb284524116880379914f2913411bb223c6941
76Trystan <trystan.frederick@gmail.com>
2026-01-15 09:17:00 +1100

    added 1 second interval to activities

--- src/temporal_direct_workflow/activities.ts
+++ src/temporal_direct_workflow/activities.ts
@@ -1,61 +1,78 @@
+const sleep = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
+
 export const activities = {
   async selfGetCup(): Promise<void> {
+    await sleep(1000);
     console.log('selfGetCup');
   },
 
   async kettleFill(): Promise<void> {
+    await sleep(1000);
     console.log('kettleFill');
   },
 
   async kettleTurnOn(): Promise<void> {
-    console.log('kettleTurnOn');
+    await sleep(1000);
+   console.log('kettleTurnOn');
   },
 
   async kettleWaitWhistle(): Promise<void> {
-    console.log('kettleWaitWhistle');
+    await sleep(1000);
+   console.log('kettleWaitWhistle');
   },
 
   async cupAddTeabag(): Promise<void> {
+    await sleep(1000);
     console.log('cupAddTeabag');
   },
 
   async cupAddWater(): Promise<void> {
+    await sleep(1000);
     console.log('cupAddWater');
   },
 
   async cupMashTea(): Promise<void> {
+    await sleep(1000);
     console.log('cupMashTea');
   },
 
   async cupRemoveTeabag(): Promise<void> {
+    await sleep(1000);
     console.log('cupRemoveTeabag');
   },
 
   async cupAddMilk(): Promise<void> {
+    await sleep(1000);
     console.log('cupAddMilk');
   },
 
   async cupAddSugar(): Promise<void> {
+    await sleep(1000);
     console.log('cupAddSugar');
   },
 
   async cupAddSalt(): Promise<void> {
+    await sleep(1000);
     console.log('cupAddSalt');
   },
 
   async cupStir(): Promise<void> {
+    await sleep(1000);
     console.log('cupStir');
   },
 
   async selfDrinkCup(): Promise<void> {
+    await sleep(1000);
     console.log('selfDrinkCup');
   },
 
   async selfEmptyCup(): Promise<void> {
+    await sleep(1000);
     console.log('selfEmptyCup');
   },
 
   async selfTidyUp(): Promise<void> {
+    await sleep(1000);
     console.log('selfTidyUp');
   },
 };
